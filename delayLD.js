af4fbd3a85e2e6020b060cc0fcddf0a709ed4eb8
Axel Born <vladadamm@gmail.com>
2022-04-03 04:37:08 +0200

    Added seconds support + hours are restricted to 24

--- delayLD.js
+++ delayLD.js
@@ -1,8 +1,7 @@
-setTimeout(CreateButtons, 1000); // Short delay to make sure all countdowns were generated
+var countdownList = document.getElementsByClassName("sidebar-countdown");
+setTimeout(CreateButtons, 1000); // Short inevitable delay to make sure all countdowns were generated
 
 function CreateButtons() {
-    var countdownList = document.getElementsByClassName("sidebar-countdown");
-
     for (let i = 0; i < countdownList.length; i++) {
         countdownList[i].style.textAlign = "center";
 
@@ -15,31 +14,32 @@ function CreateButtons() {
 
             let minNumbers = countdownList[i].getElementsByClassName("minutes-2")[0].children;
             for (let j = 0; j < minNumbers.length; j++) {
-                if (IncrementDigit(minNumbers[j], 10, j))
-                    if (IncrementDigit(countdownList[i].getElementsByClassName("minutes-1")[0].children[j], 6))
-                        if (IncrementDigit(countdownList[i].getElementsByClassName("hours-2")[0].children[j], 10))
-                            if (IncrementDigit(countdownList[i].getElementsByClassName("hours-1")[0].children[j], 6))
-                                if (IncrementDigit(countdownList[i].getElementsByClassName("days-2")[0].children[j], 10))
-                                    IncrementDigit(countdownList[i].getElementsByClassName("days-1")[0].children[j], 10);
+                if (IncrementDigit(minNumbers[j], 10))
+                    if (IncrementDigit(countdownList[i].getElementsByClassName("minutes-1")[0].children[j], 6)) {
+                        if (IncrementDigit24(countdownList[i].getElementsByClassName("hours-1")[0].children[j], countdownList[i].getElementsByClassName("hours-2")[0].children[j]))
+                            if (IncrementDigit(countdownList[i].getElementsByClassName("days-2")[0].children[j], 10))
+                                IncrementDigit(countdownList[i].getElementsByClassName("days-1")[0].children[j], 10);
+                    }
             }
         };
         countdownList[i].appendChild(button);
         button.click(); // Force timers to get blocked
     }
     DecreaseTimers();
-    setInterval(DecreaseTimers, 60000)
+    setInterval(DecreaseTimers, 1000);
 }
 
 function DecreaseTimers() {
     for (let i = 0; i < countdownList.length; i++) {
-        let minNumbers = countdownList[i].getElementsByClassName("minutes-2")[0].children;
-        for (let j = 0; j < minNumbers.length; j++) {
-            if (DecrementDigit(minNumbers[j], 10, j))
-                if (DecrementDigit(countdownList[i].getElementsByClassName("minutes-1")[0].children[j], 6))
-                    if (DecrementDigit(countdownList[i].getElementsByClassName("hours-2")[0].children[j], 10))
-                        if (DecrementDigit(countdownList[i].getElementsByClassName("hours-1")[0].children[j], 6))
-                            if (DecrementDigit(countdownList[i].getElementsByClassName("days-2")[0].children[j], 10))
-                                DecrementDigit(countdownList[i].getElementsByClassName("days-1")[0].children[j], 10);
+        let secNumbers = countdownList[i].getElementsByClassName("seconds-2")[0].children;
+        for (let j = 0; j < secNumbers.length; j++) {
+            if (DecrementDigit(secNumbers[j], 10))
+                if (DecrementDigit(countdownList[i].getElementsByClassName("seconds-1")[0].children[j], 6))
+                    if (DecrementDigit(countdownList[i].getElementsByClassName("minutes-2")[0].children[j], 10))
+                        if (DecrementDigit(countdownList[i].getElementsByClassName("minutes-1")[0].children[j], 6))
+                            if (DecrementDigit24(countdownList[i].getElementsByClassName("hours-1")[0].children[j], countdownList[i].getElementsByClassName("hours-2")[0].children[j]))
+                                if (DecrementDigit(countdownList[i].getElementsByClassName("days-2")[0].children[j], 10))
+                                    DecrementDigit(countdownList[i].getElementsByClassName("days-1")[0].children[j], 10);
         }
     }
 }
@@ -52,9 +52,44 @@ function DecrementDigit(digit, modulo) {
     }
     return (num == 0);
 }
+function DecrementDigit24(digit1, digit2) {
+    let num1 = Number(digit1.textContent);
+    let num2 = Number(digit2.textContent);
+    if (num2 == 0) {
+        if (num1 == 0) {
+            num1 = 2;
+            num2 = 3;
+        } else {
+            num1 = num1 - 1;
+            num2 = 9;
+        }
+    } else {
+        num2 = num2 - 1;
+    }
+    digit1.textContent = num1;
+    digit2.textContent = num2;
+    return (num1 == 2 && num2 == 3);
+}
 
 function IncrementDigit(digit, modulo) {
     let num = Number(digit.textContent);
     digit.textContent = (num + 1) % modulo;
     return (num == modulo - 1);
+}
+
+function IncrementDigit24(digit1, digit2) {
+    let num1 = Number(digit1.textContent);
+    let num2 = Number(digit2.textContent);
+    if (num2 == 9) {
+        num2 = 0;
+        num1 = num + 1;
+    } else if (num1 == 2 && num2 == 3) {
+        num2 = 0;
+        num1 = 0;
+    } else {
+        num2 = num2 + 1;
+    }
+    digit1.textContent = num1;
+    digit2.textContent = num2;
+    return (num1 + num2 == 0);
 }
